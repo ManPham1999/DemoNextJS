5e56aa958817e9c7db1627f3e2c82d30140fb78b
PHAM MINH MAN <man.t170670@vanlanguni.vn>
2020-09-04 14:25:47 +0700

    demo hook in next js

--- hellonext/pages/products.tsx
+++ hellonext/pages/products.tsx
@@ -1,43 +1,52 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import Axios from 'axios';
 import { useRouter } from 'next/router';
 import Product from "../Interface/Product";
+
 interface Props {
-	products?: Product[];
+    products?: Product[];
 }
-const Products = (props: Props) => {
-	const router = useRouter();
-	const { products } = props;
-	return (
-		<div>
-			<h1>this is products</h1>
-			<ul>
-				{products.map((item, index) => {
-					return <li key={index}>{item.name}</li>;
-				})}
-			</ul>
-		</div>
-	);
+
+const Products = ({ products }: Props) => {
+    const [count, setCount] = useState(0);
+    useMemo(() => {
+        console.log("memo: render 1 lần duy nhất khi khởi tạo chạy sau getInitialProps");
+    }, [])
+    const router = useRouter();
+    useEffect(() => {
+        console.log("render mỗi khi biến count thay đổi");
+    }, [count])
+    return (
+        <div>
+            <h1>this is products {count}</h1>
+            <ul>
+                {products.map((item, index) => {
+                    return <li key={index}>{item.name}</li>;
+                })}
+            </ul>
+            <button onClick={() => setCount(count + 1)}>count</button>
+        </div>
+    );
 };
 Products.getInitialProps = async (ctx) => {
-	const { query } = ctx;
-	console.log(query);
-	const response = await Axios({
-		method: 'GET',
-		url: `https://5e3995ba8d7e1300149cd550.mockapi.io/products/Products`,
-		headers: {
-			Accept: 'application/json',
-		},
-	})
-		.then((res) => {
-			return res.data;
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+    console.log("getInitialPropsrender: 1 lần duy nhất khi khởi tạo và chạy đầu tiên");
+    const { query } = ctx;
+    const response = await Axios({
+        method: 'GET',
+        url: `https://5e3995ba8d7e1300149cd550.mockapi.io/products/Products`,
+        headers: {
+            Accept: 'application/json',
+        },
+    })
+        .then((res) => {
+            return res.data;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 
-	return {
-		products: response,
-	};
+    return {
+        products: response,
+    };
 };
 export default Products;
